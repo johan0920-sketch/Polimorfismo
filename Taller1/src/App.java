be79c7748c66dedc6786d7839e6cb26639a63cb0
jorgefel <jorjoorde@gmail.com>
2025-08-28 21:04:59 -0500

    Agrego clases Caballo, Elefante y Foca que implementan Animal

--- Taller1/src/App.java
+++ Taller1/src/App.java
@@ -1,10 +1,11 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        // Polimorfismo: todas son del tipo Animal
-        Animal[] animales = { new Perro(), new Gato() };
+    public static void main(String[] args) {
+        Animal caballo = new Caballo();
+        Animal elefante = new Elefante();
+        Animal foca = new Foca();
 
-        for (Animal animal : animales) {
-            animal.hacerSonido(); // Llama al método específico de cada clase
-        }
+        caballo.hacerSonido();
+        elefante.hacerSonido();
+        foca.hacerSonido();
     }
 }
